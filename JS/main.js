da6f4fb114fe3689500605158880e6d33fa05af6
MohamedALNaser <mo7ammednaser092@gmail.com>
2022-02-26 11:42:49 +0200

    add the responseve of project-one

--- JS/main.js
+++
@@ -1,9 +0,0 @@
-let liNavArr = document.querySelectorAll("header  ul li");
-console.log(liNavArr);
-let arr = ["work", "about", "blog", "contact"];
-liNavArr.forEach(function (ele, i) {
-	ele.classList.add(arr[i]);
-	ele.innerHTML = `<a href="#${arr[i]} " style=" color: #000;font-size: 20px;text-decoration: none;"> ${arr[i]} </a>`;
-});
-let liSeleArr = document.querySelectorAll(".work .container .select ul");
-document.getElementsByClassName("selected").onclick = function () {};
